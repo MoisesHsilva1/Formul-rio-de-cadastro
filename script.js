f20f1a3fe5869d039b35b3ee20c2ff374ac069d1
MoisésHsilva1 <moihgm67@gmail.com>
2024-03-14 17:56:40 -0300

    Testando validação com JS

--- script.js
+++ script.js
@@ -1,22 +1,12 @@
-var conteudoCpf =  document.querySelector('radioCpf-conteudo');
-var conteudoRg = document.querySelector('radioRg-conteudo');
+function validarForm() {
+    let Nome = document.querySelector('.CampoNome');
 
-function Cadastrar() {
-    var conteudoCpf =  document.querySelector('radioCpf-conteudo');
-    var conteudoRg = document.querySelector('radioRg-conteudo');
-    const opcaoCpf =  document.querySelector('Campo-Cpf');
-    const opcaopRg = document.querySelector('Campo-Rg');
-
-    if(opcaoCpf.checked) {
-        conteudoCpf.style.display = "block";
-        conteudoRg.style.display = "none";
-    } else if(opcaopRg.checked) {
-        conteudoRg.style.display = "none";
-        conteudoCpf.style.display = "block";
-    } else {
-        window.alert('selecione apenas um');
+    if (Nome.value.trim() === "") {
+        window.alert('PREENCHA TODOS OS CAMPOS');
+        Nome.focus();
+        return false; 
     }
- }
-
+    return true; 
+}
 
 
