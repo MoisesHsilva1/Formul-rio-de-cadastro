145953589c25e947d130201724a1e9718f7a026e
MoisésHsilva1 <moihgm67@gmail.com>
2024-03-15 18:18:36 -0300

    Fazendo validação do RG e CPF

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-function validarForm() {
-    let Nome = document.querySelector('.CampoNome');
+function validarForm () {
+    let inputCpf = document.querySelector('.CampoCpf2');
+    
+    if(inputCpf.value > 11) {
+        window.alert('CPF TEM LIMITE DE 11 CARACTERES');
+        return false;
+    } else {
+       return true;
+    }  
+}
+function validarRG () {
+    let inputRg = document.querySelector('.Campo-Rg2');
 
-    if (Nome.value.trim() === "") {
-        window.alert('PREENCHA TODOS OS CAMPOS');
-        Nome.focus();
-        return false; 
-    }
-    return true; 
+    if (inputRg.value > 9) {
+        window.alert('RG TEM LIMITE DE 9 CARACTERES');
+        return false;
+    } else
+    return validarForm();
 }
 
-
+ document.querySelector('.button').addEventListener('click', function() {
+     validarRG();
+});
