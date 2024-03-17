35ca7831eeca4e2d4596e2be656f2e0937fdc53a
MoisésHsilva1 <moihgm67@gmail.com>
2024-03-17 04:35:46 -0300

    testando funcionalidades

--- script.js
+++ script.js
@@ -1,33 +1,61 @@
-const inputRg = document.querySelector('.Campo-Rg');
-const inputCpf = document.querySelector('.CampoCpf');
-const radioCpf = document.querySelector('.Radio-Cpf')
-const radioRg = document.querySelector('.Radio-Rg')
+const radioCpf = document.querySelector('.Radio-Cpf');
+const radioRg = document.querySelector('.Radio-Rg');
+const inputCep =  document.querySelector('.Campo-Cep');
 
-function validarForm () {
-    
+function validarCpf () {
     if(inputCpf.value.length >= 12) {
         window.alert('CPF TEM LIMITE DE 11 CARACTERES');
         return false;
     } else {
        return true;
-    }  
+    }      
 }
-function validarRG () {
+document.querySelector('.button').addEventListener('click', function() {
+    validarCpf();
+});
 
+function validarRG () {
     if (inputRg.value.length >= 10) {
         window.alert('RG TEM LIMITE DE 9 CARACTERES');
         return false;
     } else
-    return validarForm();
+    return true;
 }
-
- document.querySelector('.button').addEventListener('click', function() {
+    document.querySelector('.button').addEventListener('click', function() {
      validarRG();
 });
 
-document.getElementById('radio').forEach(function(radio) {
-    
-})
+function validarCep () {
+    if( inputCep.value.length >= 10) {
+        window.alert('CEP TEM LIMITE DE 9 CARACTERESS');
+        return false;
+    } else {
+        return true;
+    }
+}
+    document.querySelector('.button').addEventListener('click', function() {
+    validarCep();
+});
+
+  document.querySelectorAll('input[type="radio"]').forEach(function(radio) {
+        radio.addEventListener('change', function() {
+            if (this.value === "cpf") {
+              document.querySelector('.Campo-Cpf').classList.remove('hidden');
+              document.querySelector('.Campo-Cpf').setAttribute('required', true);
+              document.querySelector('.Campo-Rg').classList.add('hidden');
+              document.querySelector('.Campo-Rg').removeAttribute('required');
+            } else if (this.value === "rg") {
+             document.querySelector('.Campo-Cpf').classList.add('hidden')
+             document.querySelector('.CampoCpf').removeAttribute('required');
+             document.querySelector('.Campo-Rg').classList.remove('hidden');
+             document.querySelector('.Campo-Rg').setAttribute('required', true);
+            } 
+        });
+ });
+
+  
+
+
 
 
 
