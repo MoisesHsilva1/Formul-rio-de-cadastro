e6ff8e872db7deb11f7fcbb487691721a2c27e11
MoisésHsilva1 <moihgm67@gmail.com>
2024-04-07 13:00:39 -0300

    Atulizando validaçoes

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ function validarTelefone () {
 
 
 function validarEmail (inputEmail) {
-    if (inputEmail.indexOf(".") === -1) {
+    if (inputEmail.indexOf(".") == -1) {
         window.alert('Email incorreto. O email deve conter "."');
         return false;
     } 
@@ -66,43 +66,4 @@ document.querySelector('.button').addEventListener('click', function() {
     let inputEmail = document.querySelector('#CampoEmail').value;
     validarEmail(inputEmail);
 });
-$ (function()
-{
-    $('#cpf').blur(function()
-    {
-     let cpf = $('#cpf').val().replace(/[^0-9]/g, '').toString();
 
-      if( cpf.length == 11 )
-        {
-      var v = [];
-
-            v[0] = 1 * cpf[0] + 2 * cpf[1] + 3 * cpf[2];
-            v[0] += 4 * cpf[3] + 5 * cpf[4] + 6 * cpf[5];
-            v[0] += 7 * cpf[6] + 8 * cpf[7] + 9 * cpf[8];
-            v[0] = v[0] % 11;    
-            v[0] = v[0] % 10;
-
-            v[1] = 1 * cpf[1] + 2 * cpf[2] + 3 * cpf[3];
-            v[1] += 4 * cpf[4] + 5 * cpf[5] + 6 * cpf[6];
-            v[1] += 7 * cpf[7] + 8 * cpf[8] + 9 * v[0];
-            v[1] = v[1] % 11;
-            v[1] = v[1] % 10;
-
-        if ( (v[0] != cpf[9]) || (v[1] != cpf[10]) ) {
-             alert('CPF INVALIDO' + cpf);
-
-                $('#cpf').val('');
-                $('cpf').focus();
-          }
-         }
-         else
-        {
-        alert('cpf false' + cpf);
-
-            $('#cpf').val('');
-            $('#cpf').focus();
-        }
-    });
-});
-document.querySelector('.button').addEventListener('clik', function(){
-});
