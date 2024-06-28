65514c44fc084f0ba8b82daec2b96e8f3b8d8d20
Moises Henrique <moihgm67@gmail.com>
2024-06-28 14:02:58 -0300

    Validando Rg e Cpf

--- script.js
+++ script.js
@@ -1,71 +1,83 @@
 const radioCpf = document.querySelector('#Radio-Cpf');
 const radioRg = document.querySelector('#Radio-Rg');
-const inputCep =  document.querySelector('#Campo-Cep');
+const inputCep = document.querySelector('#Campo-Cep');
 const inputCpf = document.querySelector('#Campo-Cpf');
 const inputRg = document.querySelector('#Campo-Rg');
 const inputTelefone = document.querySelector('#CampoTelefone');
 let inputEmail = document.querySelector('#CampoEmail');
 
-function validarRG () {
+function validarRG() {
     if (inputRg.value.length <= 10) {
         window.alert('RG TEM LIMITE DE 9 CARACTERES');
         return false;
     } else
-    return true;
+        return true;
 };
-    document.querySelector('.button').addEventListener('click', function() {
-     validarRG();
+document.querySelector('.button').addEventListener('click', function () {
+    validarRG();
 });
 
-(function(){
-  
-  inputCep.addEventListener('blur', e=> {
-  		const value = inputCep.value.replace(/[^0-9]+/, '');
-      const url = `https://viacep.com.br/ws/${value}/json/`;
-      
-      fetch(url)
-      .then( response => response.json())
-      .then( json => {
-      		
-          if( json.logradouro ) {
-          	document.querySelector('#Campo-Rua').value = json.logradouro;
-            document.querySelector('#Campo-Bairro').value = json.bairro;
-            document.querySelector('#Campo-Cidade').value = json.localidade;
-          }
-      
-      });
-  });
+(function () {
+
+    inputCep.addEventListener('blur', e => {
+        const value = inputCep.value.replace(/[^0-9]+/, '');
+        const url = `https://viacep.com.br/ws/${value}/json/`;
+
+        fetch(url)
+            .then(response => response.json())
+            .then(json => {
+
+                if (json.logradouro) {
+                    document.querySelector('#Campo-Rua').value = json.logradouro;
+                    document.querySelector('#Campo-Bairro').value = json.bairro;
+                    document.querySelector('#Campo-Cidade').value = json.localidade;
+                }
+            });
+    });
 
 })();
 
-function validarTelefone () {
-    if(inputTelefone.value.length <= 12) {
+function validarTelefone() {
+    if (inputTelefone.value.length <= 12) {
         window.alert('TELEFONE INCORRETO LIMITE DE CARACTERES');
         return false;
     } else {
         return true;
     }
 }
- document.querySelector('.button').addEventListener('click',  function() {
+document.querySelector('.button').addEventListener('click', function () {
     validarTelefone();
- });
+});
 
 
-function validarEmail (inputEmail) {
+function validarEmail(inputEmail) {
     if (inputEmail.indexOf(".") == -1) {
         window.alert('Email incorreto. O email deve conter "."');
         return false;
-    } 
+    }
     if (inputEmail.includes(" ")) {
         window.alert('O Email não pode contém espaços em branco');
         return false;
     }
     return true;
 }
-document.querySelector('.button').addEventListener('click', function() {
+document.querySelector('.button').addEventListener('click', function () {
     let inputEmail = document.querySelector('#CampoEmail').value;
     validarEmail(inputEmail);
 });
+
+function RadiosAtivoCPF() {
+    if (radioCpf.checked) {
+        radioRg.disabled = false;
+
+    } else {
+        inputRg.disabled = false;
+    }
+}
+document.querySelector('#Campo-Cpf').addEventListener('change', function () {
+    RadiosAtivoCPF();
+})
+
 
 
 
