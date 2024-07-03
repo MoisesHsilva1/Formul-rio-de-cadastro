6e523c4f8f0508d6b5f1ab233f105edb359d4a74
Moises Henrique <moihgm67@gmail.com>
2024-07-02 22:10:31 -0300

    Atualizando Projeto

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const radioCpf = document.querySelector('#Radio-Cpf');
 const radioRg = document.querySelector('#Radio-Rg');
-let inputCep = document.querySelector('#Campo-Cep').value;
+let inputCep = document.querySelector('#Campo-Cep')
 let inputCpf = document.querySelector('#Campo-Cpf');
 let inputRg = document.querySelector('#Campo-Rg');
 let inputTelefone = document.querySelector('#CampoTelefone');
@@ -12,8 +12,9 @@ let CampoNumero =  document.querySelector('#Campo-Numero')
 let CampoEstados = document.querySelector('#uf') 
 let CampoCidade =  document.querySelector('#Campo-Cidade')
 let CampoComplemento = document.querySelector('#Campo-Complemento')
-const CamposEndereco = document.getElementsByClassName('formulario__campos__endereco')
+const CamposEndereco = document.getElementsByClassName('.formulario__campos__endereco')
 
+/*
 function validarRG() {
     if (inputRg.value.length <= 10) {
         window.alert('RG TEM LIMITE DE 9 CARACTERES');
@@ -24,11 +25,13 @@ function validarRG() {
 document.querySelector('.button').addEventListener('click', function () {
     validarRG();
 });
+*/
+(function() {
 
-(function () {
-
-    inputCep.addEventListener('blur', e => {
-        const value = inputCep.value.replace(/[^0-9]+/, '');
+    const cep = document.querySelector('input[name="cep"]')
+    
+    cep.addEventListener('blur', e => {
+        const value = cep.value.replace(/[^0-9]+/, '');
         const url = `https://viacep.com.br/ws/${value}/json/`;
 
         fetch(url)
@@ -36,27 +39,13 @@ document.querySelector('.button').addEventListener('click', function () {
             .then(json => {
 
                 if (json.logradouro) {
-                    document.querySelector('#Campo-Rua').value = json.logradouro;
-                    document.querySelector('#Campo-Bairro').value = json.bairro;
-                    document.querySelector('#Campo-Cidade').value = json.localidade;
+                    document.querySelector('input[name="rua"]').value = json.logradouro;
+                    document.querySelector('input[name="bairro"]').value = json.bairro;
+                    document.querySelector('input[name="cidade"]').value = json.localidade;
                 }
             });
     });
-
 })();
-
-function validarTelefone() {
-    if (inputTelefone.value.length <= 12) {
-        window.alert('TELEFONE INCORRETO LIMITE DE CARACTERES');
-        return false;
-    } else {
-        return true;
-    }
-}
-document.querySelector('.button').addEventListener('click', function () {
-    validarTelefone();
-});
-
 
 function validarEmail(inputEmail) {
     if (inputEmail.indexOf(".") == -1) {
@@ -78,16 +67,12 @@ document.querySelector('.button').addEventListener('click', function () {
 function alterarDisableRadios() {
     if (radios[0].checked) {
         inputCpf.disabled = false;
+        inputCpf.required = true;
         inputRg.disabled = true;
     } else if (radios[1].checked) {
         inputRg.disabled = false;
+        inputRg.required = true;
         inputCpf.disabled = true;
-    }
-}
-function CepHabilitaCampos () {
-    console.log('tESTNANDO')
-    if (inputCep.value == '') {
-        CampoRua.hidden = false;
     }
 }
 
